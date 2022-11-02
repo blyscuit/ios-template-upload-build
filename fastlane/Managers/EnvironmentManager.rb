dde4fe0282e440cc29545e201a880bd874b485c2
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-11-02 09:55:49 +0700

    Use value instead

--- fastlane/Managers/EnvironmentManager.rb
+++ fastlane/Managers/EnvironmentManager.rb
@@ -19,7 +19,7 @@ class EnvironmentManager
       @fastlane.sh("echo BUILD_NUMBER=#{build_number} >> $GITHUB_ENV")
     end
     if @is_bitrise
-      @fastlane.sh("echo -n #{@build_path} | envman add --key BUILD_PATH")
+      @fastlane.sh("envman add --key BUILD_PATH --value '#{@build_path}'")
     end
   end
 
