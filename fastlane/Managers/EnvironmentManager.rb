b6e712a10c21a4805190fba8493b0364e7c1d46a
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-11-04 17:04:31 +0700

    Use upload artifact instead of upload tag

--- fastlane/Managers/EnvironmentManager.rb
+++ fastlane/Managers/EnvironmentManager.rb
@@ -9,16 +9,12 @@ class EnvironmentManager
   end
 
   def save_build_context_to_ci(version_number:)
-    ipa_path =  Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::IPA_OUTPUT_PATH]
-    dsym_path = Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::DSYM_OUTPUT_PATH]
     build_number = Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]
 
     if @is_github_actions
-      @fastlane.sh("echo IPA_OUTPUT_PATH=#{ipa_path} >> $GITHUB_ENV")
-      @fastlane.sh("echo DSYM_OUTPUT_PATH=#{dsym_path} >> $GITHUB_ENV")
       @fastlane.sh("echo BUILD_NUMBER=#{build_number} >> $GITHUB_ENV")
       @fastlane.sh("echo VERSION_NUMBER=#{version_number} >> $GITHUB_ENV")
-      @fastlane.sh("echo BUILD_PATH=#{build_path} >> $GITHUB_ENV")
+      @fastlane.sh("echo BUILD_PATH=#{@build_path} >> $GITHUB_ENV")
     end
     if @is_bitrise
       @fastlane.sh("envman add --key BUILD_PATH --value '#{@build_path}'")
